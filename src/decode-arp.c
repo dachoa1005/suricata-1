a4bac613560ed0209033ef6714666df450110593
dachoa1005 <hoa.pd185357@sis.hust.edu.vn>
2023-05-31 18:15:59 +0700

    update

--- src/decode-arp.c
+++ src/decode-arp.c
@@ -12,6 +12,7 @@
 #include "util-optimize.h"
 #include "util-print.h"
 #include "util-profiling.h"
+#include "flow-hash.h"
 
 int DecodeARP(ThreadVars *tv, DecodeThreadVars *dtv, Packet *p, 
         const uint8_t *pkt, uint32_t len)
@@ -24,29 +25,29 @@ int DecodeARP(ThreadVars *tv, DecodeThreadVars *dtv, Packet *p,
         return TM_ECODE_FAILED;
     }
 
-    if (unlikely(len < ARP_HEADER_LEN)) {
-        ENGINE_SET_INVALID_EVENT(p, ARP_PKT_TOO_SMALL);
+    if (len < sizeof(ARPHdr)) {
+        // ENGINE_SET_INVALID_EVENT(p, ARP_PKT_TOO_SMALL);
         return TM_ECODE_FAILED;
     }
 
     p->arph = (ARPHdr *)pkt;
-    // p->payload = NULL;
-
-    // printf("test\n");
-
+    
     if (unlikely(p->arph == NULL))
         return TM_ECODE_FAILED;
 
-    // printf("ARP: SHA: %02x:%02x:%02x:%02x:%02x:%02x SPA: %u.%u.%u.%u -> THA: %02x:%02x:%02x:%02x:%02x:%02x TPA: %u.%u.%u.%u\n",
-    // p->arph->arp_src_mac[0], p->arph->arp_src_mac[1], p->arph->arp_src_mac[2],
-    // p->arph->arp_src_mac[3], p->arph->arp_src_mac[4], p->arph->arp_src_mac[5],
-    // p->arph->arp_src_ip[0], p->arph->arp_src_ip[1], p->arph->arp_src_ip[2],
-    // p->arph->arp_src_ip[3], p->arph->arp_des_mac[0], p->arph->arp_des_mac[1],
-    // p->arph->arp_des_mac[2], p->arph->arp_des_mac[3], p->arph->arp_des_mac[4],
-    // p->arph->arp_des_mac[5], p->arph->arp_des_ip[0], p->arph->arp_des_ip[1],
-    // p->arph->arp_des_ip[2], p->arph->arp_des_ip[3]);
+    printf("decode-arp.c, ");
+    printf("ARP: SRC MAC: %02x:%02x:%02x:%02x:%02x:%02x SRC IP: %u.%u.%u.%u -> DES MAC: %02x:%02x:%02x:%02x:%02x:%02x DES IP: %u.%u.%u.%u\n",
+    p->arph->arp_src_mac[0], p->arph->arp_src_mac[1], p->arph->arp_src_mac[2],
+    p->arph->arp_src_mac[3], p->arph->arp_src_mac[4], p->arph->arp_src_mac[5],
+    p->arph->arp_src_ip[0], p->arph->arp_src_ip[1], p->arph->arp_src_ip[2],
+    p->arph->arp_src_ip[3], p->arph->arp_des_mac[0], p->arph->arp_des_mac[1],
+    p->arph->arp_des_mac[2], p->arph->arp_des_mac[3], p->arph->arp_des_mac[4],
+    p->arph->arp_des_mac[5], p->arph->arp_des_ip[0], p->arph->arp_des_ip[1],
+    p->arph->arp_des_ip[2], p->arph->arp_des_ip[3]);
 
     // printf("ARP opcode: %d\n", ntohs(p->arph->arp_opcode));
     // printf("ARP proto size: %d\n", p->arph->arp_proto_size);
+    FlowSetupPacket(p);
+    
     return TM_ECODE_OK;
 }
